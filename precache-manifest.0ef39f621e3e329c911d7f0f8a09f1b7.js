ec2a1a18697c9d55dc1fbc7b405dbcb5ef89d84f
rudalson <rudalson@gmail.com>
2020-02-13 16:19:04 +0900

    Updates

---
+++ precache-manifest.0ef39f621e3e329c911d7f0f8a09f1b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fd65963d52dc6a210668e8b798659e",
+    "url": "/resort/index.html"
+  },
+  {
+    "revision": "00416bcd50f4cf5b1350",
+    "url": "/resort/static/css/main.86a4a10f.chunk.css"
+  },
+  {
+    "revision": "7f1fa771550c34ce4a9c",
+    "url": "/resort/static/js/2.13929983.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/resort/static/js/2.13929983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00416bcd50f4cf5b1350",
+    "url": "/resort/static/js/main.99aec754.chunk.js"
+  },
+  {
+    "revision": "2cf44f3ec608ccf08e40",
+    "url": "/resort/static/js/runtime-main.e315ad50.js"
+  },
+  {
+    "revision": "d6c88837ec4eb44101e9500319a22001",
+    "url": "/resort/static/media/jeju-pension.d6c88837.jpg"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/resort/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/resort/static/media/room-2.052c7681.jpeg"
+  }
+]);
