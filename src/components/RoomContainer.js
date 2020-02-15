38f172653f9e3581f6796bab545bba7f7a9d5edc
rudalson <rudalson@gmail.com>
2020-02-15 19:02:02 +0900

    added roomconsumer to room container
    room container 구조안에 context.js의 roomconsumer를 주입하는 부분 구현

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,13 +1,20 @@
 import React from "react";
 import RoomFilter from "./RoomFilter";
 import RoomList from "./RoomList";
+import { withRoomConsumer } from "../context";
+import Loading from "./Loading";
 
-export default function RoomContainer() {
+function RoomContainer({context}) {
+    const {loading, sortedRooms, rooms} = context;
+    if (loading) {
+        return <Loading/>;
+    }
     return (
-        <div>
-            Hello Froms Rooms Container
-            <RoomFilter/>
-            <RoomList/>
-        </div>
-    )
+        <>
+            <RoomFilter rooms={rooms}/>
+            <RoomList rooms={sortedRooms}/>
+        </>
+    );
 }
+
+export default withRoomConsumer(RoomContainer);
