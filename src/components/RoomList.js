601148b440d7a2fc3f802c7e2119a47d3537a798
rudalson <rudalson@gmail.com>
2020-02-15 19:15:49 +0900

    added the roomlist to show rooms
    room list내 rooms 보여주는 부분 구현

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Room from "./Room";
 
-export default function RoomList() {
+export default function RoomList({rooms}) {
+    if (rooms.length === 0) {
+        return (
+            <div className="empty-search">
+                <h3>해당 검색 조건에 맞는 방이 없습니다</h3>
+            </div>
+        );
+    }
     return (
-        <div>
-            Hello Froms Rooms List
-        </div>
-    )
+        <section className="roomslist">
+            <div className="roomslist-center">
+                {rooms.map(item => {
+                    return <Room key={item.id} room={item}/>;
+                })}
+            </div>
+        </section>
+    );
 }
